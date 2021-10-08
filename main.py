119575dcd22b37b100dcd891b7452de13f62b658
youngkeun_Jung <birdonmars@sasa.hs.kr>
2021-10-08 11:22:36 +0900

    writer module debug

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from pyvirtualdisplat import Display
 import os
 
 
@@ -15,77 +16,74 @@ def sendmail(name, title, contents):
     rel = '친구'
     pwd = '1234'
 
-    try:
-        options = webdriver.ChromeOptions()
-        # options.add_argument("--headless")
-        # driver_path = '/Users/yk/PycharmProjects/IPbot/chromedriver'
-        driver_path = '/home/ubuntu/base/Salute/chromedriver'
-        driver = webdriver.Chrome(driver_path, options=options)
+    display = Display(visible=0, size=(1024, 768))
+    display.start()
+    options = webdriver.ChromeOptions()
+    # options.add_argument("--headless")
+    # driver_path = '/Users/yk/PycharmProjects/IPbot/chromedriver'
+    driver_path = '/home/ubuntu/base/Salute/chromedriver'
+    driver = webdriver.Chrome(driver_path, options=options)
 
-        print("webdriver initialized")
+    print("webdriver initialized")
 
-        # ROKAF main
-        driver.get('https://atc.airforce.mil.kr:444/user/indexSub.action?codyMenuSeq=156893223&siteId=last2&menuUIType=sub')
-        print("ROKAF main1")
-        driver.find_element_by_name('searchName').send_keys(soldier_name)
-        print("ROKAF main2")
-        driver.find_element_by_xpath('/html/body/div[1]/div[2]/div/div[2]/div/div/form/div[2]/table/tbody/tr[2]/td[2]/input[1]').send_keys(birthYear)
-        print("ROKAF main3")
-        driver.find_element_by_xpath('/html/body/div[1]/div[2]/div/div[2]/div/div/form/div[2]/table/tbody/tr[2]/td[2]/input[2]').send_keys(birthMonth)
-        print("ROKAF main4")
-        driver.find_element_by_xpath('/html/body/div[1]/div[2]/div/div[2]/div/div/form/div[2]/table/tbody/tr[2]/td[2]/input[3]').send_keys(birthDay)
-        print("ROKAF main5")
-        driver.find_element_by_xpath('/html/body/div[1]/div[2]/div/div[2]/div/div/form/div[3]/span/input').click()
+    # ROKAF main
+    driver.get('https://atc.airforce.mil.kr:444/user/indexSub.action?codyMenuSeq=156893223&siteId=last2&menuUIType=sub')
+    print("ROKAF main1")
+    driver.find_element_by_name('searchName').send_keys(soldier_name)
+    print("ROKAF main2")
+    driver.find_element_by_xpath('/html/body/div[1]/div[2]/div/div[2]/div/div/form/div[2]/table/tbody/tr[2]/td[2]/input[1]').send_keys(birthYear)
+    print("ROKAF main3")
+    driver.find_element_by_xpath('/html/body/div[1]/div[2]/div/div[2]/div/div/form/div[2]/table/tbody/tr[2]/td[2]/input[2]').send_keys(birthMonth)
+    print("ROKAF main4")
+    driver.find_element_by_xpath('/html/body/div[1]/div[2]/div/div[2]/div/div/form/div[2]/table/tbody/tr[2]/td[2]/input[3]').send_keys(birthDay)
+    print("ROKAF main5")
+    driver.find_element_by_xpath('/html/body/div[1]/div[2]/div/div[2]/div/div/form/div[3]/span/input').click()
 
-        print("ROKAF main complete")
+    print("ROKAF main complete")
 
-        # Pop up
-        driver.switch_to.window(driver.window_handles[1])
-        driver.find_element_by_xpath('/html/body/div/ul/li/input').click()
+    # Pop up
+    driver.switch_to.window(driver.window_handles[1])
+    driver.find_element_by_xpath('/html/body/div/ul/li/input').click()
 
-        print("pop up")
+    print("pop up")
 
-        # Submit
-        driver.switch_to.window(driver.window_handles[0])
-        driver.find_element_by_xpath('/html/body/div[1]/div[2]/div/div[2]/div/div/form/div[3]/span/input').click()
+    # Submit
+    driver.switch_to.window(driver.window_handles[0])
+    driver.find_element_by_xpath('/html/body/div[1]/div[2]/div/div[2]/div/div/form/div[3]/span/input').click()
 
-        # Submit
-        driver.find_element_by_xpath('/html/body/div/div[2]/div[1]/div[2]/div[1]/div/div[3]/span/input').click()
+    # Submit
+    driver.find_element_by_xpath('/html/body/div/div[2]/div[1]/div[2]/div[1]/div/div[3]/span/input').click()
 
-        # Input Address
-        driver.find_element_by_xpath('/html/body/div[1]/div[2]/div/div[2]/div/div/form/div[1]/table/tbody/tr[3]/td/div[1]/span/input').click()
-        driver.switch_to.window(driver.window_handles[1])
-        driver.find_element_by_xpath('/html/body/div/div[2]/button[2]').click() # warning discard
-        driver.find_element_by_xpath('/html/body/form[2]/div/div[1]/div/div[1]/fieldset/span/input[1]').send_keys(addr)
-        driver.find_element_by_xpath('/html/body/form[2]/div/div[1]/div/div[1]/fieldset/span/input[2]').click()
-        driver.find_element_by_xpath('/html/body/form[2]/div/div[1]/div/div[2]/table/tbody/tr/td[2]/a/div/span[2]').click()
-        driver.find_element_by_xpath('/html/body/form[2]/div/div[1]/div/div[3]/table/tbody/tr[2]/td/input').send_keys(addr_spec)
-        driver.find_element_by_xpath('/html/body/form[2]/div/div[1]/div/div[3]/div/a').click()
+    # Input Address
+    driver.find_element_by_xpath('/html/body/div[1]/div[2]/div/div[2]/div/div/form/div[1]/table/tbody/tr[3]/td/div[1]/span/input').click()
+    driver.switch_to.window(driver.window_handles[1])
+    driver.find_element_by_xpath('/html/body/div/div[2]/button[2]').click() # warning discard
+    driver.find_element_by_xpath('/html/body/form[2]/div/div[1]/div/div[1]/fieldset/span/input[1]').send_keys(addr)
+    driver.find_element_by_xpath('/html/body/form[2]/div/div[1]/div/div[1]/fieldset/span/input[2]').click()
+    driver.find_element_by_xpath('/html/body/form[2]/div/div[1]/div/div[2]/table/tbody/tr/td[2]/a/div/span[2]').click()
+    driver.find_element_by_xpath('/html/body/form[2]/div/div[1]/div/div[3]/table/tbody/tr[2]/td/input').send_keys(addr_spec)
+    driver.find_element_by_xpath('/html/body/form[2]/div/div[1]/div/div[3]/div/a').click()
 
-        print("address")
+    print("address")
 
-        # Switch to main
-        driver.switch_to.window(driver.window_handles[0])
+    # Switch to main
+    driver.switch_to.window(driver.window_handles[0])
 
-        # Name
-        driver.find_element_by_id('senderName').send_keys(name)
-        # Relation
-        driver.find_element_by_xpath('/html/body/div[1]/div[2]/div/div[2]/div/div/form/div[1]/table/tbody/tr[5]/td/input').send_keys(rel)
-        # Title
-        driver.find_element_by_xpath('/html/body/div[1]/div[2]/div/div[2]/div/div/form/div[1]/table/tbody/tr[6]/td/input').send_keys(title)
-        # Contents
-        driver.find_element_by_xpath('/html/body/div[1]/div[2]/div/div[2]/div/div/form/div[1]/table/tbody/tr[7]/td/textarea').send_keys(contents)
-        # Pwd
-        driver.find_element_by_xpath('/html/body/div[1]/div[2]/div/div[2]/div/div/form/div[1]/table/tbody/tr[8]/td/input').send_keys(pwd)
+    # Name
+    driver.find_element_by_id('senderName').send_keys(name)
+    # Relation
+    driver.find_element_by_xpath('/html/body/div[1]/div[2]/div/div[2]/div/div/form/div[1]/table/tbody/tr[5]/td/input').send_keys(rel)
+    # Title
+    driver.find_element_by_xpath('/html/body/div[1]/div[2]/div/div[2]/div/div/form/div[1]/table/tbody/tr[6]/td/input').send_keys(title)
+    # Contents
+    driver.find_element_by_xpath('/html/body/div[1]/div[2]/div/div[2]/div/div/form/div[1]/table/tbody/tr[7]/td/textarea').send_keys(contents)
+    # Pwd
+    driver.find_element_by_xpath('/html/body/div[1]/div[2]/div/div[2]/div/div/form/div[1]/table/tbody/tr[8]/td/input').send_keys(pwd)
 
-        # Submit
-        driver.find_element_by_xpath('/html/body/div[1]/div[2]/div/div[2]/div/div/form/div[2]/span[1]/input').click()
+    # Submit
+    driver.find_element_by_xpath('/html/body/div[1]/div[2]/div/div[2]/div/div/form/div[2]/span[1]/input').click()
 
-        driver.quit()
-        return 1
-
-    except:
-        return 0
+    driver.quit()
 
 
 if __name__ == '__main__':
