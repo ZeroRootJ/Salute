5b4b2f21bb4b694fde08b279c33ef5321bc5a186
youngkeun_Jung <birdonmars@sasa.hs.kr>
2021-10-10 21:24:17 +0900

    writer module debug

--- main.py
+++ main.py
@@ -35,8 +35,9 @@ def sendmail(name, title, contents):
     driver.find_element_by_xpath('/html/body/div[7]/div/div[9]/div[1]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div/div/div[1]/a/h3').click()
     driver.implicitly_wait(10)
 
+    driver.save_screenshot('result1.png')
     driver.switch_to.window(driver.window_handles[1])
-    driver.save_screenshot('result.png')
+    driver.save_screenshot('result2.png')
 
     # driver.find_element_by_xpath('/html/body/div[1]/div[2]/div/div[2]/div[3]/a[3]').click()
     WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[1]/div[2]/div/div[2]/div[3]/a[3]'))).click()
