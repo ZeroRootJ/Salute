e9b086aff843998d4b1d48dcf4a6df0ab32043c1
youngkeun_Jung <birdonmars@sasa.hs.kr>
2021-10-10 20:04:17 +0900

    writer module debug

--- main.py
+++ main.py
@@ -16,11 +16,11 @@ def sendmail(name, title, contents):
     pwd = '1234'
 
     options = webdriver.ChromeOptions()
-    # options.add_argument("--headless")
+    options.add_argument("--headless")
     options.add_argument("window-size=1920x1080")
     options.add_argument("disable-gpu")
-    driver_path = '/Users/yk/PycharmProjects/IPbot/chromedriver'
-    # driver_path = '/home/ubuntu/base/Salute/chromedriver'
+    # driver_path = '/Users/yk/PycharmProjects/IPbot/chromedriver'
+    driver_path = '/home/ubuntu/base/Salute/chromedriver'
     driver = webdriver.Chrome(driver_path, options=options)
 
     print("webdriver initialized")
