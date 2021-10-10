46aa6408f6be61d8425ef0e6e075b2a0535cec53
youngkeun_Jung <birdonmars@sasa.hs.kr>
2021-10-10 20:24:49 +0900

    writer module debug

--- main.py
+++ main.py
@@ -31,7 +31,9 @@ def sendmail(name, title, contents):
     driver.find_element_by_xpath('/html/body/div[7]/div/div[9]/div[1]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div/div/div[1]/a/h3').click()
     print("2")
     driver.switch_to.window(driver.window_handles[1])
+    print(driver.current_url)
     driver.find_element_by_xpath('/html/body/div[1]/div[2]/div/div[2]/div[3]/a[3]').click()
+
 
 
     driver.find_element_by_name('searchName').send_keys(soldier_name)
