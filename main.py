0240b1505f9bc7a96e68097af77d34e426a3e3f4
youngkeun_Jung <birdonmars@sasa.hs.kr>
2021-10-10 20:36:33 +0900

    writer module debug

--- main.py
+++ main.py
@@ -27,11 +27,12 @@ def sendmail(name, title, contents):
 
     # ROKAF main
     driver.get('https://www.google.com/search?q=%EA%B8%B0%EB%B3%B8%EA%B5%B0%EC%82%AC%ED%9B%88%EB%A0%A8%EB%8B%A8&newwindow=1&sxsrf=AOaemvJrncjSP0NasmiAriUSfFMQMjvVIQ%3A1633863640083&ei=2MdiYZy_BM-U-Abu0Qo&ved=0ahUKEwici4Ov2L_zAhVPCt4KHe6oAgAQ4dUDCA4&uact=5&oq=%EA%B8%B0%EB%B3%B8%EA%B5%B0%EC%82%AC%ED%9B%88%EB%A0%A8%EB%8B%A8&gs_lcp=Cgdnd3Mtd2l6EAMyBQgAEIAEMgUIABCABDIECAAQHjIECAAQHjIECAAQHjIECAAQHjIECAAQHjIECAAQHjIECAAQHjIECAAQHjoECCMQJzoICAAQgAQQsQM6CwgAEIAEELEDEIMBOgcIIxDqAhAnOgoIABCABBCHAhAUOgQIABBDSgQIQRgAUJp1WOyKAWCgjAFoBHAAeAKAAYoCiAGnF5IBBjAuMTguMZgBAKABAbABCsABAQ&sclient=gws-wiz')
-    print("1")
+    driver.implicitly_wait(10)
+
     driver.find_element_by_xpath('/html/body/div[7]/div/div[9]/div[1]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div/div/div[1]/a/h3').click()
-    print("2")
+    driver.implicitly_wait(10)
+
     driver.switch_to.window(driver.window_handles[1])
-    print(driver.current_url)
     # driver.find_element_by_xpath('/html/body/div[1]/div[2]/div/div[2]/div[3]/a[3]').click()
     driver.find_element_by_css_selector('#popupWrap > div:nth-child(3) > a:nth-child(5)').click()
 
